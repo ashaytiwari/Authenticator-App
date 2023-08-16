544650f428bfcceb8e1a61179173be6969288646
ashaytiwari <ashaytiwari2001@gmail.com>
2023-08-16 23:11:02 +0530

    chore: added login and signup routes, installed some third party dependencies

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+function LoginPage() {
+
+  return (
+    <div>
+      <h3>Login Page</h3>
+    </div>
+  );
+
+}
+
+export default LoginPage;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,11 @@
+function SignupPage() {
+
+  return (
+    <div>
+      <h3>Sign Up Page</h3>
+    </div>
+  );
+
+}
+
+export default SignupPage;
