1698153f52a006d4c4c1d9382f0576ece570e45a
ashaytiwari <ashaytiwari2001@gmail.com>
2023-08-30 23:10:11 +0530

    feat: implemented middleware for route protection

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+// This file is responsible for handling route protection
+// Middleware will be invoked for every route in your project.
+
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+
+  const path = request.nextUrl.pathname;
+
+  const isPublicPath = path === '/login' || path === '/signup';
+
+  const token = request.cookies.get('token')?.value || '';
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL('/', request.nextUrl));
+  }
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL('/login', request.nextUrl));
+  }
+}
+
+// matching paths for middleware to run on
+export const config = {
+  matcher: [
+    '/',
+    '/login',
+    '/signup',
+    '/profile/:path*'
+  ]
+};
