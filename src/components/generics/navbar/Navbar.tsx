3e556d24ff5e1a28171fa0efd1955486117ed07f
ashaytiwari <ashaytiwari2001@gmail.com>
2023-09-02 23:52:01 +0530

    ui: created common generic navbar component

--- src/components/generics/navbar/Navbar.tsx
+++ src/components/generics/navbar/Navbar.tsx
@@ -1,20 +1,96 @@
-import React from 'react';
-import { Caveat } from 'next/font/google';
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
+
+import { PulseLoader } from 'react-spinners';
+
+import navbarLinks from '@/constants/json/navbar-links';
 
 import styles from './Navbar.module.scss';
-
-const caveat = Caveat({
-  weight: 'variable',
-  subsets: ['latin']
-});
+import { userLogout } from '@/services/auth';
 
 const Navbar = () => {
 
-  return (
-    <div className={`${caveat.className} ${styles.navbarMain}`}>
-      <h2 className={styles.logoTitle}>Authenticator <span>App</span></h2>
-      <div className={styles.navbarControls}>
+  const pathname = usePathname();
+  const router = useRouter();
 
+  const [loading, setLoading] = useState(false);
+
+  async function handleLogout() {
+
+    setLoading(true);
+
+    const response = await userLogout();
+
+    if (response!.data.statusCode === 200) {
+      router.push('/login');
+    }
+
+    setLoading(false);
+
+  }
+
+  function renderNavbarLink(item: any, index: number) {
+
+    let navbarLinkClassName = styles.navbarLink;
+
+    if (item.path === pathname) {
+      navbarLinkClassName += ` ${styles.active}`;
+    }
+
+    const linkControlAttributes = {
+      href: item.path,
+      className: navbarLinkClassName,
+      key: index
+    };
+
+    return (
+      <Link {...linkControlAttributes}>{item.label}</Link>
+    );
+  }
+
+  function renderNavbarLinks() {
+
+    return (
+      <div className={styles.navbarLinksContainer}>
+        {
+          navbarLinks.map((item, index) => (
+            renderNavbarLink(item, index)
+          ))
+        }
+      </div>
+    );
+  }
+
+  function renderLogoutControl() {
+
+    let contentNode: React.ReactNode = 'Logout';
+
+    if (loading === true) {
+
+      const pulseLoaderAttributes = {
+        color: '#fff',
+        size: 8
+      };
+
+      contentNode = <PulseLoader {...pulseLoaderAttributes} />;
+    }
+
+    const logoutButtonControlAttributes = {
+      className: 'application-themed-button',
+      disabled: loading, // disable logout button only when loading is enabled
+      onClick: handleLogout
+    };
+
+    return <button {...logoutButtonControlAttributes}>{contentNode}</button>;
+  }
+
+  return (
+    <div className={styles.navbarMain}>
+      <label className={styles.logoTitle}>Auth <span>App</span></label>
+      <div className={styles.navbarControls}>
+        {renderNavbarLinks()}
+        {renderLogoutControl()}
       </div>
     </div>
   );
---
+++ src/constants/json/navbar-links.ts
@@ -0,0 +1,20 @@
+const navbarLinks = [
+  {
+    label: 'Home',
+    path: '/'
+  },
+  {
+    label: 'About us',
+    path: '/about-us'
+  },
+  {
+    label: 'Products',
+    path: '/products'
+  },
+  {
+    label: 'Profile',
+    path: '/profile'
+  },
+];
+
+export default navbarLinks;
